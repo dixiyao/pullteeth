08f75981ba9194c9055be1e778dac4e9f8f59be9
daxixi <pspo-qfl@163.com>
2021-07-20 19:03:32 +0800

    0720 work

--- control1.py
+++ control1.py
@@ -1,28 +1,51 @@
 import socket
 import pickle
 import time
+from read_data import tooth
 
+t=tooth(r'T T_Green_Split_002.stl')
+t.transform_reverse_up_and_down()
 delegate= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 delegate.connect(('169.254.174.11',30002))
 print("connect success")
 
-data=open('data.txt')
-data=data.readlines()
-start_x=-0.39816
-start_y=-0.43976
-start_z=0.0638+0.4
-biasx=start_x+0.02557
-biasy=start_y-0.08304
-biasz=start_z-0.06501
-for line in data:
-    lines=line.split(' ')
-    x = float(lines[0])
-    y = float(lines[1])
-    z = float(lines[2])
+#初始点坐标
+start_x=-0.16477
+start_y=-0.53254
+start_z=-0.24378+0.4
+start_rx=2.3499
+start_ry=-3.0351
+start_rz=-3.1150
+biasx=start_x-t.points[0][0][0]/1000
+biasy=start_y-t.points[0][0][1]/1000
+biasz=start_z-t.points[0][0][2]/1000
+
+for i in range(t.levels):
+    level=i
+    #t.get_level(i+1)
+    #data=open('data.txt')
+    #data=data.readlines()
+    #切除一个平面
+    for j in range(0,len(t.points[level])):
+        x = round(t.points[level][j][0],2)
+        y = round(t.points[level][j][1],2)
+        z = round(t.points[level][j][2],2)
+        x = x/1000+biasx
+        y = y/1000+biasy
+        z = z/1000+biasz
+        print(x,y,z)
+        command='movel(p['+str(x)+','+str(y)+','+str(z)+','+str(start_rx)+','+str(start_ry)+','+str(start_rz)+'],a=0.01, v=0.05, t=0, r=0)\n'
+        #delegate.send(command.encode("utf8"))
+        time.sleep(0.05)
+    #移到下一层
+    if level==t.levels-1:
+        continue
+    x = round(t.points[level+1][0][0],2)
+    y = round(t.points[level+1][0][1],2)
+    z = round(t.points[level+1][0][2],2)
     x = x/1000+biasx
     y = y/1000+biasy
     z = z/1000+biasz
-    print(x,y,z)
-    command='movel(p['+str(x)+','+str(y)+','+str(z)+','+'0,0,0],a=0.01, v=0.05, t=0, r=0)\n'
-    delegate.send(command.encode("utf8"))
+    command = 'movel(p[' + str(x) + ',' + str(y) + ',' + str(z) + ',' + str(start_rx) + ',' + str(start_ry) + ',' + str(start_rz) + '],a=0.01, v=0.05, t=0, r=0)\n'
+    #delegate.send(command.encode("utf8"))
     time.sleep(0.05)
--- read_data.py
+++ read_data.py
@@ -1,63 +1,100 @@
 import vtk
 import matplotlib.pyplot as plt
 import numpy as np
+import copy
 
-#读取stl文件
-stlreader=vtk.vtkSTLReader()
-stlreader.SetFileName(r"C:\Users\Administrator\Desktop\Tooth\GetPoint2\Segmentation_Segment_1.stl")
-stlreader.Update()
+class tooth(object):
+    def __init__(self,name):
+        self.name=name
+        #读取stl文件
+        stlreader=vtk.vtkSTLReader()
+        stlreader.SetFileName(name)
+        stlreader.Update()
 
-#根据测试，x轴为牙齿长轴，故沿y轴逆时针旋转90°
-rotate1=vtk.vtkTransform()
-rotate1.RotateY(-90)
-filter1=vtk.vtkTransformPolyDataFilter()
-filter1.SetInputConnection(stlreader.GetOutputPort())
-filter1.SetTransform(rotate1)
-filter1.Update()
+        
+        #根据测试，x轴为牙齿长轴，故沿y轴逆时针旋转90°
+        rotate1=vtk.vtkTransform()
+        #rotate1.RotateY(-90)
+        filter1=vtk.vtkTransformPolyDataFilter()
+        filter1.SetInputConnection(stlreader.GetOutputPort())
+        filter1.SetTransform(rotate1)
+        filter1.Update()
+        
+        #旋转后的结果写入result.stl文件
+        stlwriter=vtk.vtkSTLWriter()
+        stlwriter.SetFileName(r"result.stl")
+        stlwriter.SetInputConnection(filter1.GetOutputPort())
+        stlwriter.Write()
+        
+        #获取点坐标，按z值划分XOY平面，间隔为2mm。
+        p=[0,0,0]
+        polydata=filter1.GetOutput()
+        points=[]
+        new_z=[]
+        
+        for i in range(polydata.GetNumberOfPoints()):
+            polydata.GetPoint(i,p)
+            z=int(p[2])
+            if p[2]-z<0 and z%2==1:
+                z=-z-1
+            if p[2]-z>=0 and z%2==1:
+                z=z+1 
+            if (z not in new_z):
+                new_z.append(z)
+        #逆序排列，表面的牙z值大
+        new_z.sort(reverse=True)
+        for i in range(0,len(new_z)):
+            tmp=[]
+            points.append(tmp)
+        for i in range(polydata.GetNumberOfPoints()):
+            polydata.GetPoint(i,p)
+            z=int(p[2])
+            if p[2]-z<0 and z%2==1:
+                z=z-1
+            if p[2]-z>=0 and z%2==1:
+                z=z+1
+            index=new_z.index(z)
+            tmp=[p[0],p[1],p[2]]
+            points[index].append(tmp)
+        # for i in range(0,len(points)):
+        #     file.write(str(new_z[i])+":  "+str(len(points[i]))+"\n")
+        #     for j in range(0,len(points[i])):
+        #         file.write(str(round(points[i][j][0],2))+" "+str(round(points[i][j][1],2))+" "+str(round(points[i][j][2],2))+'\n')
+        #     file.write("\n")
+        self.points=points
+        self.levels=len(points)
 
-#旋转后的结果写入result.stl文件
-stlwriter=vtk.vtkSTLWriter()
-stlwriter.SetFileName(r"C:\Users\Administrator\Desktop\Tooth\GetPoint2\result.stl")
-stlwriter.SetInputConnection(filter1.GetOutputPort())
-stlwriter.Write()
+    def transform_reverse_up_and_down(self):
+        points_bk=copy.deepcopy(self.points)
+        for i in range(len(self.points)):
+            self.points[i]=points_bk[len(self.points)-i-1]
+        for j in range(len(self.points)):
+            for i in range(len(self.points[j])):
+                self.points[j][i][2]=-self.points[j][i][2]
+        
+    def get_level(self,level):
+        points=self.points
+        file=open(r"data.txt",mode="w")
+        #修改获取哪一层的数据
+        level=1
+        for i in range(0,len(points[level])):
+            file.write(str(round(points[level][i][0],2))+" "+str(round(points[level][i][1],2))+" "+str(round(points[level][i][2],2))+'\n')
 
-#获取点坐标，按z值划分XOY平面，间隔为2mm。
-p=[0,0,0]
-polydata=filter1.GetOutput()
-points=[]
-new_z=[]
-file=open(r"C:\Users\Administrator\Desktop\Tooth\GetPoint2\data.txt",mode="w")
-for i in range(polydata.GetNumberOfPoints()):
-    polydata.GetPoint(i,p)
-    z=int(p[2])
-    if p[2]-z<0 and z%2==1:
-        z=z-1
-    if p[2]-z>=0 and z%2==1:
-        z=z+1 
-    if (z not in new_z):
-        new_z.append(z)
-#逆序排列，表面的牙z值大
-new_z.sort(reverse=True)
-for i in range(0,len(new_z)):
-    tmp=[]
-    points.append(tmp)
-for i in range(polydata.GetNumberOfPoints()):
-    polydata.GetPoint(i,p)
-    z=int(p[2])
-    if p[2]-z<0 and z%2==1:
-        z=z-1
-    if p[2]-z>=0 and z%2==1:
-        z=z+1
-    index=new_z.index(z)
-    tmp=[p[0],p[1],p[2]]
-    points[index].append(tmp)
-# for i in range(0,len(points)):
-#     file.write(str(new_z[i])+":  "+str(len(points[i]))+"\n")
-#     for j in range(0,len(points[i])):
-#         file.write(str(round(points[i][j][0],2))+" "+str(round(points[i][j][1],2))+" "+str(round(points[i][j][2],2))+'\n')
-#     file.write("\n")
+    def draw_level(self,level):
+        x=[]
+        y=[]
+        for i in range(0,len(self.points[level])):
+            x.append(self.points[level][i][0])
+            y.append(self.points[level][i][1])
+        plt.scatter(x,y,color='blue')
+        plt.scatter([self.points[level][0][0]],[self.points[level][0][1]],color='red')
+        plt.show()
 
-#修改获取哪一层的数据
-level=1
-for i in range(0,len(points[level])):
-    file.write(str(round(points[level][i][0],2))+" "+str(round(points[level][i][1],2))+" "+str(round(points[level][i][2],2))+'\n')
+if __name__=="__main__":
+    t=tooth(r"T T_Green_Split_002.stl")
+    t.get_level(0)
+    t.transform_reverse_up_and_down()
+    for i in range(t.levels):
+        print(t.points[i][0][2])
+
+        
